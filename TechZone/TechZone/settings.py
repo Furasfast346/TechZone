5f355f8ff8f31f7c94a7b7f2e3871120bf809030
Furasfast346 <zahar.zaytsev.2007@gmail.com>
2025-12-12 16:16:39 +0300

    Добавил картинки в каталог товаров

--- TechZone/TechZone/settings.py
+++ TechZone/TechZone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'main_page/static'),
+    os.path.join(BASE_DIR, 'catalog/static'),
+
 ]
 
 MEDIA_URL = '/media/'
